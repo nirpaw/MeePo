8bb1bd5e089e7bdc825161c36028eebc13b8fc8a
Alex Pekker <alp2003@gmail.com>
2019-02-25 17:09:07 +0200

    Added  Upload Photo in EditInfo

--- app/src/main/java/il/ac/hit/meepo/EditInfoActivity.java
+++ app/src/main/java/il/ac/hit/meepo/EditInfoActivity.java
@@ -1,11 +1,17 @@
 package il.ac.hit.meepo;
 
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -13,8 +19,13 @@ import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -22,6 +33,12 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.HashMap;
 
 import il.ac.hit.meepo.Models.User;
 
@@ -53,6 +70,11 @@ public class EditInfoActivity extends AppCompatActivity {
 
     private ImageButton mSaveBtn;
 
+    StorageReference storageReference;
+    private static final int IMAGE_REQUEST = 1;
+    private Uri imageUri;
+    private StorageTask uploadTask;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,6 +86,7 @@ public class EditInfoActivity extends AppCompatActivity {
         userId = firebaseUser.getUid();
         mDatabase = FirebaseDatabase.getInstance();
         reference = mDatabase.getReference("Users");
+        storageReference = FirebaseStorage.getInstance().getReference("uploads");
 
         mMainPhoto = findViewById(R.id.editinfo_main_photo);
         mMainPhotoEditor = findViewById(R.id.editinfo_edit_main_photo);
@@ -74,6 +97,15 @@ public class EditInfoActivity extends AppCompatActivity {
         mGenderRadioGroup = findViewById(R.id.edit_info_gender_group);
         mMale = findViewById(R.id.edit_info_gender_radio_male);
         mFemale = findViewById(R.id.edit_info_gender_radio_female);
+
+
+
+        mMainPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openImage();
+            }
+        });
 
 
 
@@ -126,11 +158,11 @@ public class EditInfoActivity extends AppCompatActivity {
                 User user = dataSnapshot.getValue(User.class);
                 mUser = dataSnapshot.getValue(User.class);
 
-                if(user.getImageURL().equals("default")){
+                if(user.getImageURL().equals("default") ){
                     mMainPhoto.setImageResource(R.mipmap.ic_launcher);
                 }else{
 
-                        Glide.with(getApplicationContext()).load(user.getImageURL()).centerCrop().into(mMainPhoto);
+                    Glide.with(getApplicationContext()).load(user.getImageURL()).centerCrop().into(mMainPhoto);
 
                 }
             }
@@ -144,5 +176,88 @@ public class EditInfoActivity extends AppCompatActivity {
 
     private void hideSoftKeyboard(){
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    }
+
+
+    private void openImage() {
+
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, IMAGE_REQUEST);
+    }
+
+    private String getFileExtension(Uri uri){
+        ContentResolver contentResolver = getApplicationContext().getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    private void uploadImage(){
+        final ProgressDialog pd = new ProgressDialog(EditInfoActivity.this);
+        pd.setMessage("Uploading");
+        pd.show();
+
+        if(imageUri != null){
+            final StorageReference fileReference = storageReference.child(System.currentTimeMillis()
+                    +"."+getFileExtension(imageUri));
+
+            uploadTask = fileReference.putFile(imageUri);
+            uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot,Task<Uri>>() {
+                @Override
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                    if(!task.isSuccessful()){
+                        throw task.getException();
+                    }
+
+                    return fileReference.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if(task.isSuccessful()){
+                        Uri downloadUri = task.getResult();
+                        String mUri = downloadUri.toString();
+
+                        reference = FirebaseDatabase.getInstance().getReference("Users").child(userId);
+                        HashMap<String, Object> map = new HashMap<>();
+                        map.put("imageURL",mUri);
+                        reference.updateChildren(map);
+
+                        pd.dismiss();
+                    }
+                    else{
+//                        Toast.makeText(getContext(), "Failed!", Toast.LENGTH_SHORT).show();
+                        pd.dismiss();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(EditInfoActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    pd.dismiss();
+                }
+            });
+        }else{
+            Toast.makeText(EditInfoActivity.this, "No image selected", Toast.LENGTH_SHORT).show();
+            pd.dismiss();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null){
+            imageUri = data.getData();
+
+            if(uploadTask != null && uploadTask.isInProgress()){
+                Toast.makeText(EditInfoActivity.this, "Upload in progress", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                uploadImage();
+            }
+        }
     }
 }
